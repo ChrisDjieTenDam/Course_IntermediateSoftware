163127f85309256491a1ad9fbcdeea43f53190a8
Chris <chris_tdam@hotmail.com>
2026-03-30 13:31:41 +0300

    FIxing the error on test_suite too

--- tests/test_models.py
+++ tests/test_models.py
@@ -13,25 +13,25 @@ from inflammation.models import daily_mean, daily_min
         ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
     ])
 
-def test_daily_mean():
+def test_daily_mean(test_mean, expected_mean):
     """Test that mean function works for an array of zeros."""
 
     # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_mean), expected_mean)
+    npt.assert_array_equal(daily_mean(np.array(test_mean)), np.array(expected_mean))
 
 @pytest.mark.parametrize( #Without parametrize, if the first test fails, pytest stops
         'test_min, expected_min',
         [
             ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
-            ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+            ([ [1, 2], [3, 4], [5, 6] ], [1, 2]),
             ([ [1,-3], [-3,2], [5,-6] ], [-3, -6]),
             ([ [-4,5], [-2,2], [1,-5] ], [-4, -5])
         ]
 )
 
-def test_daily_min():
+def test_daily_min(test_min, expected_min):
     """Test that min function works for an array of zeros."""
-    npt.assert_array_equal(daily_min(test_min), expected_min)
+    npt.assert_array_equal(daily_min(np.array(test_min)), np.array(expected_min))
 
 def test_daily_min_string():
     """Test for TypeError when passing strings"""
